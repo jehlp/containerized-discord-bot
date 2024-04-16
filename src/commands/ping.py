2ab6e59dafda15f6d73e5d102bad44c257e7e8bc
jehlp <jehlpxyz@gmail.com>
2024-04-16 16:36:25 -0400

    better ping

--- src/commands/ping.py
+++ src/commands/ping.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 # Not part of stdlib
 from discord.ext import commands
 
@@ -7,7 +9,11 @@ from ..cog import DiscordCog
 class Ping(DiscordCog):
     @commands.command(name='ping')
     async def command(self, ctx):
-        await self.send_response(ctx, 'pong')
+        start_time = datetime.now()
+        message = await ctx.send('🏓 Pong!')
+        end_time = datetime.now()
+        elapsed_time = (end_time - start_time).total_seconds() * 1000
+        await message.edit(content=f'🏓 Pong! Response time: {elapsed_time:.2f} ms')
 
 async def setup(bot):
     await bot.add_cog(Ping(bot))
