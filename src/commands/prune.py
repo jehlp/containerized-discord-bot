c10c67f01b6b7dc9b92d4b708fc7fd2baa24adf2
tony <legendcrazyman@gmail.com>
2024-04-19 11:29:51 -0400

    fix ytmp4, add prune command

---
+++ src/commands/prune.py
@@ -0,0 +1,18 @@
+# Not part of stdlib
+from discord.ext import commands
+
+# Internal
+from src.cog import DiscordCog
+
+class Prune(DiscordCog):
+    @commands.command(name='prune')
+    async def command(self, ctx, num_to_delete: int = 0):
+        num_to_delete = min(num_to_delete, 100)
+        await ctx.send(content=f'Pruning last {num_to_delete} messages...')
+        await ctx.channel.purge(limit = num_to_delete + 1)
+
+    def help(self):
+        return "Removes the last specified number of messages in the channel. Requires manage message and read message history permissions in the channel. Usage: !prune #"
+
+async def setup(bot):
+    await bot.add_cog(Prune(bot))
--- src/commands/youtube.py
+++ src/commands/youtube.py
@@ -1,5 +1,4 @@
 import os
-from threading import Thread
 
 # Not part of stdlib
 from pytube import YouTube
@@ -10,8 +9,13 @@ from discord.ext import commands
 # Internal
 from src.cog import DiscordCog
 
-def download_file(stream, file_path, file_name):
-    stream.download(output_path=file_path, filename=file_name, max_retries=10)
+async def send_file(file, ctx):
+    with open(file, 'rb') as file:
+        await ctx.send(file=discord.File(file))
+
+async def download_file(stream, file_d, file_name, ctx):
+    file = stream.download(output_path=file_d, filename=file_name, max_retries=10)
+    await send_file(file, ctx)
 
 def on_progress(stream, chunk, bytes_remaining):
     print(f"Fetching {stream} --> Bytes remaining: {bytes_remaining}")
@@ -22,35 +26,29 @@ class YoutubeMP4(DiscordCog):
         format = "mp4"
         yt = None
 
-        def send_file(stream, file_path):
-            with open(file_path, 'rb') as file:
-                ctx.send(file=discord.File(file), timeout=90, max_retries=10)
-
         try:
-            yt = YouTube(url, on_progress_callback=on_progress, on_complete_callback=send_file)
+            yt = YouTube(url, on_progress_callback=on_progress, allow_oauth_cache=True)
         except PytubeError:
             await ctx.send(content=f'Invalid URL!')
             return
 
-        # progressive mode must be true
-        filter_streams = yt.streams.filter(file_extension=format, progressive=True)
-        stream = filter_streams.get_by_resolution(quality)
+        print("Youtube Object retrieved")
+        stream = yt.streams.get_by_resolution(quality)
 
         if not stream:
             await ctx.send(content='No matching stream of quality found, choosing lower quality one...')
-            stream = filter_streams.get_lowest_resolution()
+            stream = yt.streams.get_lowest_resolution()
 
         file_name = "temp_video."+format
-        file_d = os.getcwd()
-        file_path = os.path.join(file_d, file_name)
+        file_dir = os.getcwd()
+        file_path = os.path.join(file_dir, file_name)
 
         # remove temp_video if it exists 
         if os.path.exists(file_path):
             os.remove(file_path)
 
-        # use another thread to avoid blocking further commands
-        thread = Thread(target=download_file, args = (stream, file_d, file_name))
-        thread.start()
+        print("Starting download")
+        await download_file(stream, file_dir, file_name, ctx)
 
     def help(self):
         return "Takes a youtube url and returns a mp4 of specified quality (720p default). Usage: !ytmp4 <url> <quality>"
