70e10c517047be750ddc8d37a04fcd2397e965af
toeneeoh <toeneeoh@gmail.com>
2024-04-19 13:08:32 -0400

    add mp3 to youtubeDL command

--- src/commands/youtube.py
+++ src/commands/youtube.py
@@ -20,10 +20,13 @@ async def download_file(stream, file_d, file_name, ctx):
 def on_progress(stream, chunk, bytes_remaining):
     print(f"Fetching {stream} --> Bytes remaining: {bytes_remaining}")
 
-class YoutubeMP4(DiscordCog):
-    @commands.command(name='ytmp4')
-    async def command(self, ctx, url: str = None, quality: str = "720p"):
-        format = "mp4"
+class YoutubeDL(DiscordCog):
+    @commands.command(name='yt')
+    async def command(self, ctx, url: str = None, format: str = "mp4", quality: str = "720p"):
+        if format != "mp4" and format != "mp3":
+            await ctx.send(content=f'Invalid file format! Expected: mp3|mp4')
+            return
+
         yt = None
 
         try:
@@ -33,17 +36,23 @@ class YoutubeMP4(DiscordCog):
             return
 
         print("Youtube Object retrieved")
-        stream = yt.streams.get_by_resolution(quality)
+
+        match format:
+            case "mp4":
+                stream = yt.streams.get_by_resolution(quality)
+            case "mp3":
+                stream = yt.streams.get_audio_only()
 
         if not stream:
-            await ctx.send(content='No matching stream of quality found, choosing lower quality one...')
-            stream = yt.streams.get_lowest_resolution()
+            print("Could not retrieve stream")
+            return
 
-        file_name = "temp_video."+format
+        file_name = "yt_download." + format
         file_dir = os.getcwd()
         file_path = os.path.join(file_dir, file_name)
+        print(file_path)
 
-        # remove temp_video if it exists 
+        # remove temp yt_download file
         if os.path.exists(file_path):
             os.remove(file_path)
 
@@ -51,7 +60,7 @@ class YoutubeMP4(DiscordCog):
         await download_file(stream, file_dir, file_name, ctx)
 
     def help(self):
-        return "Takes a youtube url and returns a mp4 of specified quality (720p default). Usage: !ytmp4 <url> <quality>"
+        return "Takes a youtube url and returns a file of specified type (mp3|mp4). Defaults to mp4. Usage: !yt <url> <type>"
 
 async def setup(bot):
-    await bot.add_cog(YoutubeMP4(bot))
+    await bot.add_cog(YoutubeDL(bot))
