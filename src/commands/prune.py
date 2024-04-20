299f455b9083e8aa4ad83fb6eaccb9621e594f66
toeneeoh <toeneeoh@gmail.com>
2024-04-20 12:32:25 -0400

    remove bot response and issued cmd

--- src/commands/prune.py
+++ src/commands/prune.py
@@ -9,7 +9,7 @@ class Prune(DiscordCog):
     async def command(self, ctx, num_to_delete: int = 0):
         num_to_delete = min(num_to_delete, 100)
         await ctx.send(content=f'Pruning last {num_to_delete} messages...')
-        await ctx.channel.purge(limit = num_to_delete + 1)
+        await ctx.channel.purge(limit = num_to_delete + 2) # include issued command and bot response
 
     def help(self):
         return "Removes the last specified number of messages in the channel. Requires manage message and read message history permissions in the channel. Usage: !prune #"
