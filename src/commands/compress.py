cdd32d39c2e237301eec21b8230a66b851a0b108
jehlp <jehlpxyz@gmail.com>
2024-04-30 21:47:49 -0400

    strange whitespace issue

--- src/commands/compress.py
+++ src/commands/compress.py
@@ -6,27 +6,26 @@ import zipfile
 from discord.ext import commands
 
 class Compress(src.cog.DiscordCog):
-	@commands.command(name='compress')
-	async def command(self, ctx):
-		if not ctx.message.attachments:
-			return
+    @commands.command(name="compress")
+    async def command(self, ctx):
+        if not ctx.message.attachments:
+            return
+        try:
+            attachment = ctx.message.attachments[0]
+            data = io.BytesIO(await attachment.read())
+            zbuffer = io.BytesIO()
 
-		try:
-			attachment = ctx.message.attachments[0]
-			data = io.BytesIO(await attachment.read())
-			zbuffer = io.BytesIO()
+            with zipfile.ZipFile(zbuffer, "w", zipfile.ZIP_DEFLATED) as z:
+                z.writestr(attachment.filename, data.getvalue())
 
-			with zipfile.ZipFile(zbuffer, 'w', zipfile.ZIP_DEFLATED) as z:
-				z.writestr(attachment.filename, data.getvalue())
-
-			if src.utils.general.file_is_too_large(zbuffer):
-				await ctx.send(f"Compressed file is too large to upload!")
-			else:	
-				zbuffer.seek(0)
-				zipped_file = discord.File(fp=zbuffer, filename=f'{attachment.filename}.zip')
-				await ctx.send(file=zipped_file)
-		except Exception as e:
-			await ctx.send(f"Unable to compress file: {e}")
+            if src.utils.general.file_is_too_large(zbuffer):
+                await ctx.send(f"Compressed file is too large to upload!")
+            else:
+                zbuffer.seek(0)
+                zipped_file = discord.File(fp=zbuffer, filename=f"{attachment.filename}.zip")
+                await ctx.send(file=zipped_file)
+        except Exception as e:
+            await ctx.send(f"Unable to compress file: {e}")
 
 async def setup(bot):
-	await bot.add_cog(Compress(bot))
+    await bot.add_cog(Compress(bot))
